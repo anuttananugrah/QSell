6558d5b0ed8c7429a17d919a71bb75a37c7af4a6
ANUGRAH <anuttananugrah@gmail.com>
2026-04-08 00:09:08 +0530

    icon

--- rehub/urls.py
+++ rehub/urls.py
@@ -19,6 +19,8 @@ from django.urls import path,include
 from account.views import HomeView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,5 +32,9 @@ urlpatterns = [
     path('chat/', include('chat.urls')),
     path('reports/', include('report.urls')),
     path('custom-admin/', include('custom_admin.urls', namespace='custom_admin')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/logo.jpg'))),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
