42f85f32c9b869c7c87f8dbb865947916d5d9136
ANUGRAH <anuttananugrah@gmail.com>
2026-04-08 15:20:36 +0530

    catogory error

--- categories/urls.py
+++ categories/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import CategoryProductListView
 
 urlpatterns = [
-    path('category/<slug:slug>/', CategoryProductListView.as_view(), name='category_products'),
+    path('<slug:slug>/', CategoryProductListView.as_view(), name='category_products'),
 ]
