200efaa06b82d8e920d6d2ef428cf14d4514b6a9
ANUGRAH <anuttananugrah@gmail.com>
2026-04-09 13:26:26 +0530

    coudinary added

--- product/models.py
+++ product/models.py
@@ -3,6 +3,7 @@ from account.models import User
 from categories.models import Category
 from django.utils import timezone
 from datetime import timedelta
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -58,7 +59,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to="product_images/")
+    image = CloudinaryField('image')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.product.title
